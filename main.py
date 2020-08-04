3c6bb6ae8b2e4d4f8b32f4e6d0f6ebec903b9ec2
Sabit400 <sakib9197@gmail.com>
2020-08-04 23:45:16 +0600

    Add n before print

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 n = int(input())
 
 if n % 2 != 0:
-    print("Weird")
+    print(n,"Weird")
 else:
     if n >=2 and n <= 5:
-        print("Not Weird")
+        print(n,"Not Weird")
     elif n >= 6 and n <= 20:
-        print ("Weird")
+        print (n,"Weird")
     else:
-        print("Not Weird")
+        print(n,"Not Weird")
